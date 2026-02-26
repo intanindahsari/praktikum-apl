11ccbd9e7fd1909ebcaefe1f298cc41d1c4e75aa
intanindahsari <intanindah025@gmail.com>
2026-02-26 16:26:38 +0800

    finish

---
+++ kelas/Pertemuan1/pertemuan1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nama;
+    cout << "Hello,  " << endl;
+    cout << "Masukan nama anda: ";
+    cin >> nama; 
+    cout << "nama saya adalah" << nama;
+return 0;
+}
+
+// int main() {
+//     if ( 1 == 1)
+//     {
+//         cout << "satu ya satu";
+
+//     }else if ( 1 == 2){
+//         cout << "satu adalah 2";
+//     }else {
+//         cout << "kosong";
+//     }
+// return 0;
+
+// }
---
+++ kelas/Pertemuan1/tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+
+using namespace std;
+
+int main() {
+    string nama;
+    cout << "Hello, World! " << endl;
+    cout << "Belajar C + sangat seru😀";
+    cin >> nama; 
+    cout << "nama saya adalah" << nama;
+return 0;
+}
